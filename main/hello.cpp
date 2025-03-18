bc1ed737a7e9c7fc29a32e77839bae7904f91cba
Poorna <nightfury20320@gmail.com>
2025-03-18 15:13:52 +0530

    Initial commit

---
+++ main/hello.cpp
@@ -0,0 +1,13 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Hello, Jenkins!" << endl;
+    cout << "I have successfully built and run this program." << endl;
+    cout << "Developed by: Poorna Pragnya.H" << endl;
+    cout << "SRN: PES1UG22CS417" << endl;
+    return 0;
+}
+
